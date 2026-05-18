40ee2f62f6f93a401799ce2ff6c230ae40413723
Ashwin Gopale <ashwingopale2005@gmail.com>
2026-05-18 21:03:44 +0530

    sdfe

--- WAD/server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-// serve files from public folder
-app.use(express.static('public'))
-
-app.listen(3000, function() {
-    console.log('server started on port 3000')
-})
